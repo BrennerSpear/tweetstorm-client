b9ddceb07d2757e787e04b02f3e9c21cde2e1632
Brenner Spear <blspear@gmail.com>
2017-04-23 18:50:46 -0700

    Add profpic placeholder
    - Also comment out notification code

--- navigation/RootNavigation.js
+++ navigation/RootNavigation.js
@@ -12,8 +12,8 @@ import Router from './Router'
 
 import Alerts from '../constants/Alerts'
 import Colors from '../constants/Colors'
-import registerForPushNotificationsAsync
-  from '../api/registerForPushNotificationsAsync'
+// import registerForPushNotificationsAsync
+  // from '../api/registerForPushNotificationsAsync'
 
 export default class RootNavigation extends React.Component {
 
@@ -22,11 +22,11 @@ export default class RootNavigation extends React.Component {
   }
 
   componentDidMount() {
-    this._notificationSubscription = this._registerForPushNotifications()
+    // this._notificationSubscription = this._registerForPushNotifications()
   }
 
   componentWillUnmount() {
-    this._notificationSubscription && this._notificationSubscription.remove()
+    // this._notificationSubscription && this._notificationSubscription.remove()
   }
 
   render() {
@@ -64,25 +64,25 @@ export default class RootNavigation extends React.Component {
     )
   }
 
-  _registerForPushNotifications() {
-    // Send our push token over to our backend so we can receive notifications
-    // You can comment the following line out if you want to stop receiving
-    // a notification every time you open the app. Check out the source
-    // for this function in api/registerForPushNotificationsAsync.js
-    registerForPushNotificationsAsync()
+  // _registerForPushNotifications() {
+  //   // Send our push token over to our backend so we can receive notifications
+  //   // You can comment the following line out if you want to stop receiving
+  //   // a notification every time you open the app. Check out the source
+  //   // for this function in api/registerForPushNotificationsAsync.js
+  //   registerForPushNotificationsAsync()
 
-    // Watch for incoming notifications
-    this._notificationSubscription = Notifications.addListener(
-      this._handleNotification
-    )
-  }
+  //   // Watch for incoming notifications
+  //   this._notificationSubscription = Notifications.addListener(
+  //     this._handleNotification
+  //   )
+  // }
 
-  _handleNotification = ({ origin, data }) => {
-    this.props.navigator.showLocalAlert(
-      `Push notification ${origin} with data: ${JSON.stringify(data)}`,
-      Alerts.notice
-    )
-  }
+  // _handleNotification = ({ origin, data }) => {
+  //   this.props.navigator.showLocalAlert(
+  //     `Push notification ${origin} with data: ${JSON.stringify(data)}`,
+  //     Alerts.notice
+  //   )
+  // }
 }
 
 const styles = StyleSheet.create({
--- screens/NewTweetScreen.js
+++ screens/NewTweetScreen.js
@@ -23,11 +23,11 @@ export default class NewTweetScreen extends React.Component {
     console.log('NewTweetScreen componentDidMount')
   }
 
-  _renderIcon(name) {
+  _renderIcon(name, size) {
     return (
       <FontAwesome
         name={name}
-        size={24}
+        size={size}
         color='#58AEF0'
       />
     )
@@ -37,24 +37,36 @@ export default class NewTweetScreen extends React.Component {
     console.log('rendering NewTweetScreen')
     return (
       <View style={styles.container}>
+
         <View style={styles.topView}>
+
           <View style={styles.topViewLeft}>
-            <View style={styles.profPic} />
+            <View style={styles.profPicContainer}>
+              <Image style={styles.profPicImage} source={require('../assets/icons/notification-icon.png')}/>
+            </View>
+
             <View style={styles.center}>
               <Text>@{this.props.profileInfo.username}</Text>
             </View>
           </View>
+
           <View style={styles.xbutton}>
-          {this._renderIcon('times')}
+          {this._renderIcon('times', iconSize)}
           </View>
+
         </View>
+
+
         <View style={styles.mainArea}>
-        <Text> Main area</Text>
+          <Text> Main area</Text>
         </View>
+
       </View>
     )
   }
 }
+
+const iconSize = 32
 
 const styles = StyleSheet.create({
   container: {
@@ -65,17 +77,25 @@ const styles = StyleSheet.create({
   topView: {
     flexDirection: 'row',
     // backgroundColor: 'red',
-    height: 30
+    height: 50
   },
   topViewLeft: {
     flex: 1,
     flexDirection: 'row',
     // backgroundColor: 'green',
   },
-  profPic: {
+  profPicContainer: {
     // backgroundColor: 'skyblue',
-    // height: 50,
-    width: 10
+    justifyContent: 'center',
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+  profPicImage: {
+    height: iconSize,
+    width: iconSize,
+    resizeMode: 'contain'
   },
   center: {
     flex: 1,
